e364a3980598d56ce0b4479a954e4bc553a1eb2c
gabriel-marchal <55120288+gabriel-marchal@users.noreply.github.com>
2019-11-17 16:46:08 -0600

    Imagen al recinto es obligatorio
    Es obligatorio poner imagen

--- api/models/recinto.model.js
+++ api/models/recinto.model.js
@@ -12,7 +12,7 @@ const recintoSchema = new mongoose.Schema(
         provincia: { type: String, required: true, unique: false },
         canton: { type: String, required: true, unique: false },
         distrito: { type: String, required: true, unique: false },
-        imagen: { type: String, required: false, unique: false },
+        imagen: { type: String, required: true, unique: false },
         latitud: { type: Number },
         longitud: { type: Number },
 
