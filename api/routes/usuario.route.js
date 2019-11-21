4f7c17de037968e562f2f6791eb5183bf8d7021c
Karina-Rodriguez-Proyecto <krodriguezf@ucenfotec.ac.cr>
2019-11-21 16:47:41 -0600

    Cambio  del estilo del correo y el registrar correo

--- api/routes/usuario.route.js
+++ api/routes/usuario.route.js
@@ -71,6 +71,9 @@ router.post('/registrar-usuario', function (req, res) {
                                 font-family: 'Roboto', sans-serif;
                                 font-size: 14px;
                                 color: #000;
+                                width: 500px;
+                                margin-left: 30%;
+                                margin-top: 5%;
                             }
                     
                             h1 {
@@ -88,6 +91,21 @@ router.post('/registrar-usuario', function (req, res) {
                             span{
                                 color: #F2610A;
                             }
+                    
+                            h1{
+                                margin-top: -50px;
+                                margin-bottom: -10px;
+                            }
+                            h4{
+                                text-align: center;
+                            }
+                    
+                            .footer{
+                                color: #a7a4a4;
+                                margin-bottom: -8px;
+                                font-size: 12px;
+                            }
+                    
                         </style>
                     
                         <title>Cuerpo del correo</title>
@@ -95,14 +113,27 @@ router.post('/registrar-usuario', function (req, res) {
                     
                     <body>
                     
-                        <h1>Bienvenido a Ticket pixel</h1>
-                        <h2>La mejor manera de comprar entradas en linea</h2>
+                        <div>
+                            <img src="https://res-console.cloudinary.com/proyecto1-nebula/thumbnails/v1/image/upload/v1573759788/dWdrZ3dwbWFyZ2dqZ2l6OWVoeW0=/preview" style="height: 75px; margin-left: 25px; padding-top: 10px;" >
+                            <h1>Bienvenido a Ticket pixel</h1>
+                            <h4>La mejor manera de comprar entradas en linea</h4>
+                        </div>
+                        <hr>
+                        
+                        
                     
                         <div class="info_credenciales">
-                            <p>Saludos ${nuevoUsuario.nombre} le agradecemos por escoger utilizar los sevicios de Ticket pixel</p>
-                            <p>Correo electrónico asociado: <span> ${nuevoUsuario.correo} </span> </p>
+                            <p>Saludos ${nuevoUsuario.primerNombre}:</p>
+                            <p>Puedes ingresar esta contraseña para iniciar sesión en Ticket pixel:</p>
+                            <br>
+                            <p>Correo electrónico asociado: <span>${nuevoUsuario.correo} </span> </p>
                             <p>Su contraseña temporal es:  <span> ${nuevoUsuario.contrasenna} </span></p>
                         </div>
+                        <br>
+                        <br>
+                        <hr>
+                        <p class="footer">Este mensaje se envió a ${nuevoUsuario.correo}</p>
+                        <p class="footer">Equipo Nebula, Cenfotec 2019 </p>
                     
                     </body>
                     
