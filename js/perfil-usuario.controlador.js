e27066cccf60561f35a4ec03d60e32fa36a1d81e
Karina-Rodriguez-Proyecto <krodriguezf@ucenfotec.ac.cr>
2019-11-22 11:22:38 -0600

    Perfil de usuario cambios

--- js/perfil-usuario.controlador.js
+++ js/perfil-usuario.controlador.js
@@ -1,12 +1,57 @@
 'use strict'
 
-const imagen_usuario = document.querySelector('#imagen-usuario');
-const fechaDeNacimiento = document.querySelector('fecha-de-nacimiento');
-const edad = document.querySelector('edad');
-const genero = document.querySelector('genero');
-const nombre = document.querySelector('nombre');
-const direccion = document.querySelector('direccion');
-const correo = document.querySelector('correo');
-const tarjetas = document.querySelector('tarjetas');
-const intereses = document.querySelector('intereses');
+const foto_perfil_container = document.querySelector('#foto_perfil_container');
+const nombre_container = document.querySelector('#nombre_container');
+const informacion_container = document.querySelector('#informacion_container');
+const direccion_container = document.querySelector('direccion_container');
 
+let mostrarInfo = async () => {
+
+    for (let i = 0; i < listarUsuarios.length; i++) {
+
+        foto_perfil_container.style.backgroundImage = 'url, (`${imagen}`)';
+        let img = document.createElement('img');
+        img.src = `${imagen}`;
+    
+
+        let nombre = document.createElement('h3');
+        nombre.innerText =  listarUsuarios[i]['primerNombre'];
+        nombre_container.appendChild(nombre);
+
+
+        let cumpleannos = document.createElement('p');
+        cumpleannos.innerText = listarUsuarios[i]['fechaDeNacimiento'];
+
+        let correo = document.createElement('p');
+        correo.innerText = listarUsuarios[i]['correo'];
+
+        let genero = document.createElement('p');
+        genero.innerText = listarUsuarios[i]['genero'];
+
+        informacion_container.appendChild(cumpleannos);
+        informacion_container.appendChild(correo);
+        informacion_container.appendChild(genero);
+
+        let direccion = document.createElement('p');
+        direccion.innerText = listarUsuarios[i]['direccion'];
+
+        let provincia = document.createElement('p');
+        provincia.innerText = listarUsuarios[i]['provincia'];
+
+        let canton = document.createElement('p');
+        canton.innerText = listarUsuarios[i]['canton'];
+
+        let distrito = document.createElement('p');
+        distrito.innerText = listarUsuarios[i]['distrito'];
+
+
+        direccion_container.appendChild(direccion);
+        direccion_container.appendChild(provincia);
+        direccion_container.appendChild(canton);
+        direccion_container.appendChild(distrito);
+
+    }
+
+};
+
+mostrarCards();
