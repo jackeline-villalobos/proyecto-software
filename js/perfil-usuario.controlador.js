12480e196ebf028bf0690a91522c07ab023f6a57
Karina-Rodriguez-Proyecto <krodriguezf@ucenfotec.ac.cr>
2019-11-22 12:53:39 -0600

    Update perfil-usuario.controlador.js

--- js/perfil-usuario.controlador.js
+++ js/perfil-usuario.controlador.js
@@ -4,6 +4,9 @@ const foto_perfil_container = document.querySelector('#foto_perfil_container');
 const nombre_container = document.querySelector('#nombre_container');
 const informacion_container = document.querySelector('#informacion_container');
 const direccion_container = document.querySelector('direccion_container');
+const _id = sessionStorage.getItem('idUsuario');
+
+
 
 let mostrarInfo = async () => {
 
