641864d7b907f2a1c43f1a5f35e108cd126905d6
angel-vargas-cenfotec <55120114+angel-vargas-cenfotec@users.noreply.github.com>
2019-11-22 16:42:39 -0600

    Update listar recintos

--- js/listar-recintos-controlador-card.js
+++ js/listar-recintos-controlador-card.js
@@ -3,53 +3,51 @@
 const contenedor = document.querySelector('#cards-container');
 const inputFiltro = document.querySelector('#txt-filtro');
 
-let mostrarCards = async() => {
-    let filtro = inputFiltro.value.toLowerCase();
+let mostrarCards = async () => {
     let listaRecintos = await listarRecintos();
-    console.log(listaRecintos);
 
     contenedor.innerHTML = '';
 
-    for(let i = 0; i < listaRecintos.length; i++){
+    for (let i = 0; i < listaRecintos.length; i++) {
         let nombre = listaRecintos[i]['nombreRecinto'].toLowerCase();
         let imagen = listaRecintos[i]['imagen'];
-        if(nombre.includes(filtro)) {
-            let divCard = document.createElement('div');
-            divCard.classList.add('card');
 
-            let header = document.createElement('header');
-            header.style.backgroundImage = 'url, (`${imagen}`)';
-            //console.log(imagen);
-            let img = document.createElement('img');
-            img.src = `${imagen}`;            
+        let divCard = document.createElement('div');
+        divCard.classList.add('card');
 
-            let titulo = document.createElement('h2');
-            titulo.innerText = listaRecintos[i]['nombreRecinto'];
+        let header = document.createElement('header');
+        header.style.backgroundImage = 'url, (`${imagen}`)';
+        //console.log(imagen);
+        let img = document.createElement('img');
+        img.src = `${imagen}`;
 
-            let provincia = document.createElement('h3');
-            provincia.innerText = listaRecintos[i]['provincia'];
+        let titulo = document.createElement('h2');
+        titulo.innerText = listaRecintos[i]['nombreRecinto'];
 
-            let capacidad = document.createElement('h4');
-            capacidad.innerText = 'Capacidad: ' +  listaRecintos[i]['capacidad'] + ' personas';
+        let provincia = document.createElement('h3');
+        provincia.innerText = listaRecintos[i]['provincia'];
 
-            let boton = document.createElement('button');
-            boton.classList.add('btn-mas');
-            boton.innerHTML = 'Ver más';
-            boton.dataset._id = listaRecintos[i]['_id'];
+        let capacidad = document.createElement('h4');
+        capacidad.innerText = 'Capacidad: ' + listaRecintos[i]['capacidad'] + ' personas';
 
-            boton.addEventListener('click', function() {
-                sessionStorage.setItem('idRecinto', this.dataset._id);
-                window.location.href = 'perfil-recinto.html';
-            });
+        let boton = document.createElement('button');
+        boton.classList.add('btn-mas');
+        boton.innerHTML = 'Ver más';
+        boton.dataset._id = listaRecintos[i]['_id'];
 
-            contenedor.appendChild(divCard);
-            divCard.appendChild(header);
-            header.appendChild(img);
-            divCard.appendChild(titulo);
-            divCard.appendChild(provincia);
-            divCard.appendChild(capacidad);
-            divCard.appendChild(boton);
-        }
+        boton.addEventListener('click', function () {
+            sessionStorage.setItem('idRecinto', this.dataset._id);
+            window.location.href = 'perfil-recinto.html';
+        });
+
+        contenedor.appendChild(divCard);
+        divCard.appendChild(header);
+        header.appendChild(img);
+        divCard.appendChild(titulo);
+        divCard.appendChild(provincia);
+        divCard.appendChild(capacidad);
+        divCard.appendChild(boton);
+
     }
 };
 
