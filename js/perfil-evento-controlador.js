7da5e3f03d4a82d322c9e712326d05549320634c
jackeline-villalobos <jvillalobosm@ucenfotec.ac.cr>
2019-11-23 21:23:40 -0600

    perfil evento wip2

--- js/perfil-evento-controlador.js
+++ js/perfil-evento-controlador.js
@@ -3,5 +3,28 @@
 let idEvento = sessionStorage.getItem('idEvento');
 
 const imagen = document.querySelector('#imagen');
-const nombreEvento = document.querySelector('#h2-nombreEvento');
-const descripcion = document.querySelector('#p-descripcion');
+const h2_nombreEvento = document.querySelector('#h2-nombreEvento');
+const p_descripcion = document.querySelector('#p-descripcion');
+
+const div_fechas = document.querySelector('#div-fechas')
+
+let llenarEvento = async () =>{
+
+    let evento = await buscarEvento(idEvento);
+
+    console.log(evento);
+
+    let imagenSource = evento.evento.imagen;
+    imagen.src = `${imagenSource}`;
+
+    let nombreEvento = evento.evento.nombre;
+    h2_nombreEvento.innerHTML = nombreEvento;
+
+    let descripcion = evento.evento.descripcion;
+    p_descripcion.innerHTML = descripcion;
+
+    
+
+};
+
+llenarEvento();
