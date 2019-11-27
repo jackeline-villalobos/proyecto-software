7dcd7c8f532e7bd4fc06dcb40c099837aff6eb25
angel-vargas-cenfotec <55120114+angel-vargas-cenfotec@users.noreply.github.com>
2019-11-26 23:29:23 -0600

    Correo landing - Fixed

--- api/routes/encargados-validar.route.js
+++ api/routes/encargados-validar.route.js
@@ -4,15 +4,15 @@ const express = require('express');
 const router = express.Router();
 const usuarioApi = require('./encargados.api');
 
-router.route('/registrar-encargado')
-    .post(function(req, res){
-        encargadoApi.registrar(req, res);
-    });
+// router.route('/registrar-encargado')
+//     .post(function(req, res){
+//         encargadoApi.registrar(req, res);
+//     });
 
-router.route('/validar-credenciales')
-.post(function(req, res){
-    encargadoApi.validar(req, res);
-});
+// router.route('/validar-credenciales')
+// .post(function(req, res){
+//     encargadoApi.validar(req, res);
+// });
 
 
-module.exports = router;  
+// module.exports = router;  
