3559c0333de141977a325c3fc10bc7f32debf9fe
jackeline-villalobos <jvillalobosm@ucenfotec.ac.cr>
2019-11-27 18:54:26 -0600

    perfil empresas

--- js/perfil-empresa-controlador.js
+++ js/perfil-empresa-controlador.js
@@ -1,96 +1,70 @@
 'use strict';
 
 const nombreEmpresa_container = document.querySelector('#nombreEmpresa_container');
+const razonSocial_container = document.querySelector('#razonSocial_container');
 const cedulaJuridica_container = document.querySelector('#cedulaJuridica_container');
-const experiencia_container = document.querySelector('#experiencia_container');
-const nombreComercial_container = document.querySelector('#nombreComercial_container');
+const telefono_container = document.querySelector('#telefono_container');
+const correo_container = document.querySelector('#correo_container');
 
 const provincia_container = document.querySelector('#provincia_container');
 const canton_container = document.querySelector('#canton_container');
 const distrito_container = document.querySelector('#distrito_container');
-const sennas_container = document.querySelector('#sennas_container');
 
-const nombreCompleto_container = document.querySelector('#nombre_container');
-const correo_container = document.querySelector('#correo_container');
-const telefono_container = document.querySelector('#telefono_container');
-const genero_container = document.querySelector('#genero_container');
 
 const contenedor = document.querySelector('#contenedorCards');
 
 
 let mostrarInfo = async () =>{
 
-    let organizador = await obtenerDatos();
-    //console.log(organizador);
+    let empresa = await obtenerDatos();
+    console.log(empresa);
 
-    let nombreEmpresa = organizador.organizador.nombreEmpresa;
+    let nombreEmpresa = empresa.empresa.nombreEmpresa;
     let pNombreEmpresa = document.createElement('h6');
     pNombreEmpresa.innerText = nombreEmpresa;
     nombreEmpresa_container.appendChild(pNombreEmpresa);
 
-    let cedulaJuridica = organizador.organizador.cedulaJuridica;
+    let razonSocial = empresa.empresa.razonSocial;
+    let pRazonSocial = document.createElement('h6');
+    pRazonSocial.innerText = razonSocial;
+    razonSocial_container.appendChild(pRazonSocial);
+
+    let cedulaJuridica = empresa.empresa.cedulaJuridica;
     let pCedulaJuridica = document.createElement('h6');
     pCedulaJuridica.innerText = cedulaJuridica;
     cedulaJuridica_container.appendChild(pCedulaJuridica);
-
-    let experiencia = organizador.organizador.experiencia;
-    let pExperiencia = document.createElement('h6');
-    pExperiencia.innerText = experiencia;
-    experiencia_container.appendChild(pExperiencia);
-
-    let nombreComercial = organizador.organizador.nombreComercial;
-    let pNombreComercial = document.createElement('h6');
-    pNombreComercial.innerText = nombreComercial;
-    nombreComercial_container.appendChild(pNombreComercial);
-
-    let provincia = organizador.organizador.provincia;
-    let pProvincia = document.createElement('h6');
-    pProvincia.innerText = provincia;
-    provincia_container.appendChild(pProvincia);
-
-    let canton = organizador.organizador.canton;
-    let pCanton = document.createElement('h6');
-    pCanton.innerText = canton;
-    canton_container.appendChild(pCanton);
-    
-    let distrito = organizador.organizador.distrito;
-    let pDistrito = document.createElement('h6');
-    pDistrito.innerText = distrito;
-    distrito_container.appendChild(pDistrito);
-
-    let sennas = organizador.organizador.sennas;
-    let pSennas = document.createElement('h6');
-    pSennas.innerText = sennas;
-    sennas_container.appendChild(pSennas);
-
-    let nombreCompleto = organizador.organizador.nombreCompleto;
-    let pNombreCompleto = document.createElement('h6');
-    pNombreCompleto.innerText = nombreCompleto;
-    nombreCompleto_container.appendChild(pNombreCompleto);
-    
-    let correo = organizador.organizador.correo;
+        
+    let correo = empresa.empresa.correo;
     let pCorreo = document.createElement('h6');
     pCorreo.innerText = correo;
     correo_container.appendChild(pCorreo);
 
-    let telefono = organizador.organizador.telefono;
+    let telefono = empresa.empresa.telefono;
     let pTelefono = document.createElement('h6');
     pTelefono.innerText = telefono;
     telefono_container.appendChild(pTelefono);
 
-    let genero = organizador.organizador.genero;
-    let pGenero = document.createElement('h6');
-    pGenero.innerText = genero;
-    genero_container.appendChild(pGenero);
+    let provincia = empresa.empresa.provincia;
+    let pProvincia = document.createElement('h6');
+    pProvincia.innerText = provincia;
+    provincia_container.appendChild(pProvincia);
 
+    let canton = empresa.empresa.canton;
+    let pCanton = document.createElement('h6');
+    pCanton.innerText = canton;
+    canton_container.appendChild(pCanton);
     
-    
+    let distrito = empresa.empresa.distrito;
+    let pDistrito = document.createElement('h6');
+    pDistrito.innerText = distrito;
+    distrito_container.appendChild(pDistrito);
+
 };
 
 let mostrarCards = async ()=>{
-    let organizador = await obtenerDatos();
+    let empresa = await obtenerDatos();
     let listaEventos = await listarEventos();
-    let filtro = organizador.organizador._id;
+    let filtro = empresa.empresa._id;
 
     //console.log(filtro);
     
@@ -98,7 +72,7 @@ let mostrarCards = async ()=>{
         let creador = listaEventos[i]['creador'];
         let imagen = listaEventos[i]['imagen'];
        
-        console.log(listaEventos[i]['creador'])
+        //console.log(listaEventos[i]['creador'])
         if (creador.includes(filtro)) {
             
             let cardDiv = document.createElement('div');
