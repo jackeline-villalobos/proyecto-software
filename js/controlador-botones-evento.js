080081d16ac39b189b6c13496f7de12fad148bbc
jackeline-villalobos <jvillalobosm@ucenfotec.ac.cr>
2019-11-30 09:54:00 -0600

    Perfil evento nuevo Done

--- js/controlador-botones-evento.js
+++ js/controlador-botones-evento.js
@@ -7,6 +7,7 @@ console.log(grado);
 if (grado == 2 || grado == 4 || grado == null) {
     listaBotones[0].classList.add('ocultar');
     listaBotones[1].classList.add('ocultar');
+    listaBotones[2].classList.add('ocultar');
 } else {
     listaBotones[0].classList.remove('ocultar');
     listaBotones[1].classList.remove('ocultar');
