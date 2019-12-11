960488bdfa5e6c758cff33703308d5fa7aa856c8
gabriel-marchal <55120288+gabriel-marchal@users.noreply.github.com>
2019-12-10 19:57:07 -0600

    Update reporte-ingresos-controlador.js

--- js/reporte-ingresos-controlador.js
+++ js/reporte-ingresos-controlador.js
@@ -8,46 +8,43 @@ const inputTotalIngresos = document.querySelector("#txt-totalIngresos");
 
 
 let llenarTabla = async() => {
-    let filtro = inputFiltro.value.toLowerCase();
     listaEventos = await listarEventos();
-    //console.log(listaEventos);
 
     tableBody.innerHTML = '';
 
-    let totalIngreso;
-
+    let totalIngreso = 0;
+    console.log(totalIngreso);
     for (let i = 0; i < listaEventos.length; i++) {
-        //let evento = listaEventos[i]['evento'].toLowerCase();  // Esta vara no lo ocupo
 
         let precio = parseFloat(listaEventos[i]["precioEntrada"]);
 
-       // console.log(listaEventos);
-
         let fechasArray = listaEventos[i]['fechas'];
-        // console.log(fechasArray);
+
+        let totalAsistentes = 0;
         for (let j = 0; j < fechasArray.length; j++) {
             let fechasEvento = listaEventos[i]['fechas'][j];
             //console.log(fechasEvento);
-            let cantidadAsistentes = listaEventos[i]['fechas'][j]['cantidadAsistentes'];
+            let cantidadAsistentes = parseFloat(listaEventos[i]['fechas'][j]['cantidadAsistentes']);
             console.log(cantidadAsistentes);
-            // let totalAsistentes = totalAsistentes + cantidadAsistentes;
-
-
+            totalAsistentes = totalAsistentes + cantidadAsistentes;
+            console.log(totalAsistentes);
         }
 
-        //let cantidadAsistentes = parseFloat(listaEventos[i]["cantidadAsistentes"]); // Esto lo pase al for loop j 
-        //let totalEvento = totalAsistentes * listaEventos[i]["precioEntradas"];
+
+        let totalEvento = totalAsistentes * precio;
+
 
         let fila = tableBody.insertRow();
         //let entradas = (listaEventos[i]['entradas vendidas']);
         fila.insertCell().innerHTML = listaEventos[i]['nombre'];
-        //fila.insertCell().innerHTML = totalAsistentes;
-        fila.insertCell().innerHTML = precio;
-        //fila.insertCell().innerHTML = totalEvento;
+        fila.insertCell().innerHTML = totalAsistentes;
+        fila.insertCell().innerHTML = "₡" + precio;
+        fila.insertCell().innerHTML = "₡" + totalEvento;
 
-        //totalIngreso = totalIngreso + totalEvento;
+        totalIngreso = totalIngreso + totalEvento;
     }
-    //inputTotalIngresos.innerHTML = totalIngreso;
+    console.log(totalIngreso);
+    inputTotalIngresos.innerHTML = totalIngreso;
 
 }
 
