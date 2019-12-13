117cd2dfdf7b0e91cffd7777da49528709afaff4
Karina-Rodriguez-Proyecto <krodriguezf@ucenfotec.ac.cr>
2019-12-12 19:51:03 -0600

    Cambio de contraseña en los routes

--- api/routes/encargado.route.js
+++ api/routes/encargado.route.js
@@ -10,7 +10,7 @@ const transporter = nodeMailer.createTransport({
     service: 'gmail',
     auth: {
         user: 'equiponebula2019@gmail.com',
-        pass: 'krashcenfo'
+        pass: 'krashcenfo1'
     }
 });
 
